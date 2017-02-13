3ce4b0b604214a223cc59e408a89b168b72ae2f1
bishorup.banjara@outlook.com <[bishorup.banjara@gmail.com]>
2017-02-13 00:49:51 -0500

    Project Intial Component Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { EventListComponent } from './events/event-list/event-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventListComponent
   ],
   imports: [
     BrowserModule,
